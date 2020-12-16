e87a4803484a5523bcf138824063c3235328e41b
unknown <aloha.hat.2508@gmail.com>
2020-12-16 15:37:52 +0700

    add security and client side page

--- src/main/java/com/codegym/case4/controller/SecurityController.java
+++ src/main/java/com/codegym/case4/controller/SecurityController.java
@@ -1,10 +1,17 @@
 package com.codegym.case4.controller;
 
+import com.codegym.case4.model.Book;
+import com.codegym.case4.model.Category;
 import com.codegym.case4.model.Role;
 import com.codegym.case4.model.User;
+import com.codegym.case4.service.Book.IBookService;
+import com.codegym.case4.service.Category.ICategoryService;
 import com.codegym.case4.service.Role.IRoleService;
 import com.codegym.case4.service.User.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,10 +21,13 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 @Controller
 public class SecurityController {
+    @Autowired
+    private IBookService bookService;
 
     @Autowired
     private IUserService userService;
@@ -28,14 +38,38 @@ public class SecurityController {
     @Autowired
     PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private ICategoryService categoryService;
+
+    @ModelAttribute("allCategories")
+    public Iterable<Category> getAllCategories() {
+        return categoryService.findAll();
+    }
+
     @RequestMapping("/")
-    public String homePage() {
-        return "welcomePage"; //Sẽ tạo giao diện client home để thay vào.
+    public ModelAndView listBooks(@RequestParam("s") Optional<String> s, @PageableDefault(size = 10) Pageable pageable) {
+        Page<Book> books;
+        if (s.isPresent()) {
+            books = bookService.findAllByTitleContainingAndDeletedIsFalse(s.get(), pageable);
+        } else {
+            books = bookService.findAll(pageable);
+        }
+        ModelAndView modelAndView = new ModelAndView("/homeClient");
+        modelAndView.addObject("books", books);
+        return modelAndView;
     }
 
     @RequestMapping("/client")
-    public String userInfo() {
-        return "userInfoPage";
+    public ModelAndView listBooksC(@RequestParam("s") Optional<String> s, @PageableDefault(size = 10) Pageable pageable) {
+        Page<Book> books;
+        if (s.isPresent()) {
+            books = bookService.findAllByTitleContainingAndDeletedIsFalse(s.get(), pageable);
+        } else {
+            books = bookService.findAll(pageable);
+        }
+        ModelAndView modelAndView = new ModelAndView("/homeClient");
+        modelAndView.addObject("books", books);
+        return modelAndView;
     }
 
     @RequestMapping("/admin")
@@ -52,7 +86,7 @@ public class SecurityController {
 
     @PostMapping("/create")
     public ModelAndView saveCustomer(@Validated @ModelAttribute("user") User user, BindingResult bindingResult) {
-        ModelAndView modelAndView = new ModelAndView("/create");
+        ModelAndView modelAndView = new ModelAndView("create");
         if (!bindingResult.hasFieldErrors()) {
             Role role = roleService.findRoleByRoleName("ROLE_USER");
             Set<Role> roles = new HashSet<>();
@@ -60,7 +94,7 @@ public class SecurityController {
             user.setRoles(roles);
             user.setPassword(passwordEncoder.encode(user.getPassword()));
             userService.save(user);
-            modelAndView.addObject("user", user);
+            modelAndView.addObject("user", new User());
             modelAndView.addObject("message", "New user created successfully");
         }
         return modelAndView;
@@ -68,12 +102,13 @@ public class SecurityController {
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String loginPage() {
-        return "loginPage";
+        return "login";
     }
 
     @RequestMapping(value = "/logoutSuccessful", method = RequestMethod.GET)
     public String logoutSuccessfulPage(Model model) {
         model.addAttribute("title", "Logout");
-        return "welcomePage";
+        return "logoutSuccessfulPage";
     }
+
 }
